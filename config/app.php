1da7fcca47340e306270b9e27f9d92f6c7de3174
DavMijan <davmijan99@gmail.com>
2025-06-01 16:08:21 -0600

    Correct-app

--- config/app.php
+++ config/app.php
@@ -123,8 +123,4 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'providers' => [
-        // Otros providers...
-        App\Providers\ViteServiceProvider::class,
-    ],
 ];
